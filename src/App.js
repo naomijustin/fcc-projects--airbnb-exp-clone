76651868afc7a42ca707f51ec31412b11b1c402a
Naomi Justin <naomi.z.justin@gmail.com>
2023-07-05 07:19:47 +1000

    added data and console logging correctly

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import experiences from "./data.js"
+console.log(experiences);
+
+/*
+Challenge:
+
+- import the array of data from data.js
+- map over the array to create <Card /> components
+- display the array of card components under the navbar
+  (in place of the current <Card /> component)
+
+Note: We haven't styled the group of components yet, so they'll
+still be block elements, stacked vertically. We'll add styling later.
+*/
 
 function App() {
   return (
