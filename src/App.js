0fcdeddc95e34d689b6c7040f869235f28d29573
Naomi Justin <naomi.z.justin@gmail.com>
2023-07-06 21:02:31 +1000

    css with overflow scroll and absolute position working

--- src/App.js
+++ src/App.js
@@ -15,32 +15,15 @@ const cardsExp = experiences.map(item => {
             price={item.price}
           />);
 })
-/*
-Challenge:
-
-- import the array of data from data.js
-- map over the array to create <Card /> components
-- display the array of card components under the navbar
-  (in place of the current <Card /> component)
-
-Note: We haven't styled the group of components yet, so they'll
-still be block elements, stacked vertically. We'll add styling later.
-*/
 
 function App() {
   return (
     <div className="app--container">
       <Navbar />
       <Hero />
-      {/* <Card 
-        img={require(`${"./images/katie-zaferes.png"}`)}
-        rating="5.0"
-        reviewCount={6}
-        country="USA"
-        title="Life lessons with Katie Zaferes"
-        price={136}
-      />  */}
-      {cardsExp}
+      <div className="cards-contain">
+        {cardsExp}
+      </div>     
     </div>
     
   )
